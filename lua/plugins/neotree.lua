84f37eebfe09495a2fa9652f68d86ec99d644ab5
victor-prodrigues <victorrodriguesqf@gmail.com>
2024-01-13 20:28:19 -0300

    Adding close action keymap

--- lua/plugins/neotree.lua
+++ lua/plugins/neotree.lua
@@ -3,11 +3,12 @@ return   {
   branch = "v3.x",
   dependencies = {
     "nvim-lua/plenary.nvim",
-    "nvim-tree/nvim-web-devicons", 
+    "nvim-tree/nvim-web-devicons",
     "MunifTanjim/nui.nvim",
   },
   config = function()
-   vim.keymap.set('n', '<C-n>', ':Neotree filesystem reveal left<CR>', {})
+    vim.keymap.set('n', '<C-n>', ':Neotree filesystem reveal left<CR>', {})
+    vim.keymap.set('n', '<C-m>', ':Neotree close<CR>', {})
   end
 }
 
